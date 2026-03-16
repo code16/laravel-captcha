495e98e68a9b48aba235d51580c5abb3d6fd4a58
antoine <guingand.antoine@gmail.com>
2026-03-16 19:21:59 +0100

    improvements

--- resources/views/components/turnstile/widget.blade.php
+++ resources/views/components/turnstile/widget.blade.php
@@ -2,31 +2,52 @@
 <div x-data="{
         widgetId: null,
         token: null,
+        intersecting: false,
         load() {
+            if(this.widgetId) {
+                turnstile.remove(this.widgetId);
+            }
             this.widgetId = turnstile.render(this.$el, {
                 sitekey: this.$el.getAttribute('data-sitekey'),
-                theme: 'auto',
+                theme: this.$el.getAttribute('data-theme'),
                 callback: (token) => {
                     this.token = token;
                 },
                 'expired-callback': () => {
-                    turnstile.reset(this.widgetId);
-                }
+                    this.reset();
+                },
+                'response-field-name': 'captcha',
             })
+        },
+        reset() {
+            turnstile.reset(this.widgetId);
         },
         init() {
             new IntersectionObserver((entries, observer) => {
+                this.intersecting = entries[0].isIntersecting;
                 if(entries[0].isIntersecting) {
                     if(this.widgetId)  {
-                        turnstile.reset(this.widgetId);
+                        this.reset();
                     } else {
                         this.load();
                     }
                 }
             }).observe(this.$el.closest('form') || this.$el);
 
+            this.$nextTick(() => {
+                new MutationObserver(() => {
+                    this.intersecting && this.load();
+                }).observe(this.$el, { attributes: true, attributeFilter: ['data-theme'] });
+            });
+
             this.$wire?.on('reset-captcha', () => {
-                turnstile.reset(this.widgetId);
+                this.token = null;
+            });
+
+            this.$watch('token', (token) => {
+                if(!token && this.widgetId) {
+                    this.reset();
+                }
             });
         }
     }"
--- src/Rules/Captcha.php
+++ src/Rules/Captcha.php
@@ -4,16 +4,33 @@ namespace Code16\Captcha\Rules;
 
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\Validator;
 use RyanChandler\LaravelCloudflareTurnstile\Rules\Turnstile;
 
 class Captcha implements ValidationRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if(config('captcha.provider') === 'turnstile') {
-            new Turnstile()->validate($attribute, $value, $fail);
-        } else {
-            throw new \Exception('Captcha provider not found');
+        if(!config('captcha.enabled')) {
+            return;
+        }
+
+        $validator = Validator::make(
+            [$attribute => $value],
+            [
+                $attribute => [
+                    'required',
+                    match(config('captcha.provider')) {
+                        'turnstile' => new Turnstile(),
+                    }
+                ]
+            ]
+        );
+
+        if ($validator->fails()) {
+            foreach ($validator->errors()->get($attribute) as $message) {
+                $fail($message);
+            }
         }
     }
 }
