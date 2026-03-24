0f45ffddeca3726bfa8a41e550644517a9140382
antoine <guingand.antoine@gmail.com>
2026-03-24 11:54:11 +0100

    Add console.error for failed script

--- resources/views/components/turnstile/scripts.blade.php
+++ resources/views/components/turnstile/scripts.blade.php
@@ -2,6 +2,7 @@
 <script
     src="https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit"
     defer
+    onerror="console.error('Turnstile script failed to load.')"
     {{ $attributes }}
 ></script>
 
