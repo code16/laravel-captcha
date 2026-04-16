24d6b39643e35b5136924646be05ca0558fea62e
antoine <guingand.antoine@gmail.com>
2026-04-16 15:22:16 +0200

    extract onerror in separate script

--- resources/views/components/turnstile/scripts.blade.php
+++ resources/views/components/turnstile/scripts.blade.php
@@ -1,9 +1,13 @@
 
+@php($nonce = \Illuminate\Support\Facades\Vite::cspNonce())
+
 <script
+    id="turnstile-script"
     src="https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit"
     defer
-    onerror="console.error('Turnstile script failed to load.')"
-    {{ $attributes }}
+    {{ $attributes->merge(['nonce' => $nonce]) }}
 ></script>
 
-
+<script {{ $attributes->merge(['nonce' => $nonce]) }}>
+    document.getElementById('turnstile-script').addEventListener('error', () => console.error('Turnstile script failed to load.'));
+</script>
